f0fc85ef9a8a7dc3d401e860e29b9b5b5f9ffb3c
mlindauer <marius.rks@googlemail.com>
2017-09-26 09:08:39 +0200

    MAINT
    * resize image
    * more statistics (vbs, sbs)
    * test validation (leave-one-out)

--- dlbbo/dlbbo.py
+++ dlbbo/dlbbo.py
@@ -1,5 +1,7 @@
 import glob
 import os
+import typing
+import logging
 
 import scipy
 import numpy as np
@@ -21,6 +23,9 @@ class DLBBO(object):
         ''' Constructor '''
 
         self.scenario_dn = scenario_dn
+        self.logger = logging.getLogger("DLBBO")
+        
+        self.IMAGE_SIZE = 64 # pixel
 
     def main(self):
         '''
@@ -29,6 +34,7 @@ class DLBBO(object):
 
         scenario, inst_image_map = self.read_data()
 
+        sum_qual = 0
         for test_inst in scenario.instances:
             print(test_inst)
             X_train, y_train, X_test, y_test, n_classes = \
@@ -39,7 +45,17 @@ class DLBBO(object):
             y_pred = self.train(X_train=X_train, y_train=y_train,
                                 X_test=X_test, y_test=y_test,
                                 n_classes=n_classes)
-            print(y_pred)
+            for p in y_pred: # several images for each test instance:
+                print(p)
+                pred_algo_idx = np.argmax(p)
+                qual = scenario.performance_data.ix[test_inst,pred_algo_idx]
+                print(qual)
+                sum_qual += qual
+        self.logger.info("Average Quality: %f" %(sum_qual/len(scenario.instances)))
+        vbs = scenario.performance_data.min(axis=1).sum()
+        sbs = scenario.performance_data.sum(axis=0).min()
+        self.logger.info("VBS: %f" %(vbs))
+        self.logger.info("SBS: %f" %(sbs))
 
     def read_data(self):
         '''
@@ -59,12 +75,16 @@ class DLBBO(object):
         for inst in scenario.instances:
             inst_image_map[inst] = []
             for img_fn in glob.glob(os.path.join(self.scenario_dn, "images", inst + "*")):
-                inst_image_map[inst].append(scipy.misc.imread(
-                    img_fn, flatten=True) / 256)  # scale to [0,1]
+                image = scipy.misc.imread(img_fn, flatten=True)
+                # resize
+                image = scipy.misc.imresize(image, size=(self.IMAGE_SIZE,self.IMAGE_SIZE))
+                # scale 
+                image = image / 256 # 256bit grey scale
+                inst_image_map[inst].append(image)  # scale to [0,1]
 
         return scenario, inst_image_map
 
-    def build_train_data(self, scenario, inst_image_map, test_inst):
+    def build_train_data(self, scenario:ASlibScenarioDL, inst_image_map:typing.Dict, test_inst:str):
         '''
             generate X,y from scenario data
         '''
@@ -74,22 +94,23 @@ class DLBBO(object):
         n_classes = len(scenario.algorithms)
         for inst in scenario.instances:
             for image in inst_image_map[inst]:
+                X = np.reshape(image, (self.IMAGE_SIZE, self.IMAGE_SIZE, 1))
+                perfs = scenario.performance_data.loc[inst].values
+                y = np.argmin(perfs)
                 if inst != test_inst:
-                    X_train.append(np.reshape(image, (128, 128, 1)))
-                    perfs = scenario.performance_data.loc[inst].values
-                    y_train.append(np.argmin(perfs))
+                    X_train.append(X)
+                    y_train.append(y)
                 else:
-                    X_test.append(np.reshape(image, (128, 128, 1)))
-                    perfs = scenario.performance_data.loc[inst].values
-                    y_test.append(np.argmin(perfs))
+                    X_test.append(X)
+                    y_test.append(y)
 
         return np.array(X_train), np.array(y_train), \
             np.array(X_test), np.array(y_test), \
             n_classes
 
-    def train(self, X_train, y_train,
-              X_test, y_test,
-              n_classes):
+    def train(self, X_train:np.ndarray, y_train:np.ndarray,
+              X_test:np.ndarray, y_test:np.ndarray,
+              n_classes:int):
         '''
             train model
         '''
@@ -101,7 +122,7 @@ class DLBBO(object):
         # input: 128x128 images with 1 channel -> (128, 128, 1) tensors.
         # this applies 32 convolution filters of size 3x3 each.
         model.add(Conv2D(32, (3, 3), activation='relu',
-                         input_shape=(128, 128, 1)))
+                         input_shape=(self.IMAGE_SIZE, self.IMAGE_SIZE, 1)))
         model.add(Conv2D(32, (3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         # model.add(Dropout(0.25))
